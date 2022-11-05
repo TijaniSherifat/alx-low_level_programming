c9ba081962f4b64452d0b023944adab1dd795968
TijaniSherifat <sherrywumgenetics@gmail.com>
2022-11-05 07:54:33 -0700

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - a program that multiplies two numbers
+ * @argc: the number of command-line arguments
+ * @argv: an array of strings containing the command-line arguments
+ * Return: returns 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
