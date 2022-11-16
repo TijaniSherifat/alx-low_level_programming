45904a90afabe119c0a3c78a8466e3b93e98658d
TijaniSherifat <sherrywumgenetics@gmail.com>
2022-11-16 08:39:28 -0800

    Task 3

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * get_op_func - finds operation function
+ * @s: operator (+, -, *, /, %)
+ * Return: function pointer
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (i < 5 && s != NULL)
+	{
+		if (s[0] == *(ops[i].op))
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
